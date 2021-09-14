d94e17872a92a6540fc8caa973469a5d7075a1a9
Dan <danyost23@gmail.com>
2021-09-13 21:50:22 -0400

    Added templates for the index and garden pages

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Oh look it's a beautiful zen garden</p>"
+    return render_template("index.html")
+
+@app.route("/garden/<sessionID>")
+def garden(sessionID=None):
+    return render_template("garden.html")
