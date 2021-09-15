a26b6782d84eaa9c39d02c6631649ab98591c80f
Dan <danyost23@gmail.com>
2021-09-15 19:58:30 -0400

    Fixed URL generation for JS scripts

--- serverManager.py
+++ serverManager.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask import render_template
+from flask import url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("base.html", scene="index")
+    return render_template("base.html",
+            scene="index",
+            jsdir=url_for("static", filename="js"));
 
 @app.route("/garden/<sessionID>")
 def garden(sessionID=None):
-    return render_template("base.html", scene="garden")
+    return render_template("base.html",
+            scene="garden", 
+            jsdir=url_for("static", filename="js"));
