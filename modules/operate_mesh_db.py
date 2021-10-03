87e477c4b55287de5f1b9b460c505bb066209d00
FangzhengDu <47069945+FangzhengDu@users.noreply.github.com>
2021-10-03 16:13:32 -0400

    add import json

--- modules/operate_mesh_db.py
+++ modules/operate_mesh_db.py
@@ -2,19 +2,31 @@ from zengarden_database import db, Mesh
 
 
 # Add -------------------------------------------------------------
-m = Mesh(vertices="", colors="")
+def main_add3():
+    print('import data')
+    result = []
+    with open('json address', encoding='utf-8') as f:
+        result.append(json.load(f))
+        if result != None:
+            for r in result:
+                for i in r:
+                    try:
+                        me = Mesh(vertices=i['vertices'], colors=i['colors'])
+                        db.session.add(me)
+                    except:
+                        db.session.delete(me)
+                db.session.commit()
 
-
-db.session.add(m)
-db.session.commit()
+                #return render_template('success', content='import success')
+            #return ''
 # add all
 # db.session.add_all()
 # db.session.commit()
 
 # Check ------------------------------------------------------------
 # get
-mes = Mesh.query.get()
-print(mes.vertices)
+#mes = Mesh.query.get()
+#print(mes.vertices)
 
 # check all
 # mes = Mesh.query.all()
@@ -32,13 +44,13 @@ print(mes.vertices)
 
 # Change -----------------------------------------------------------
 
-mes = Mesh.query.filter(Mesh.vertices == "").update({"": ""})
-print(mes) # numbers changed
-db.session.commit()
+#mes = Mesh.query.filter(Mesh.vertices == "").update({"": ""})
+#print(mes) # numbers changed
+#db.session.commit()
 
 
 # Delete -----------------------------------------------------------
 
-mes = Mesh.query.filter(Mesh.vertices == "").delete()
+#mes = Mesh.query.filter(Mesh.vertices == "").delete()
 
-db.session.commit()
+#db.session.commit()
