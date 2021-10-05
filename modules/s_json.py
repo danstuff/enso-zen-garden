398d282b841cb8a5bffa5430a43c3ea58e57b0c5
FangzhengDu <47069945+FangzhengDu@users.noreply.github.com>
2021-10-05 14:37:06 -0400

    Update s_json.py

--- modules/s_json.py
+++ modules/s_json.py
@@ -1,5 +1,5 @@
-import json
-from zengarden_database import db, Mesh, Dialogue, Entities
+#import json
+#from zengarden_database import db, Mesh, Dialogue, Entities
 
 #gar = {
     #'Water level': water_level,
@@ -9,36 +9,36 @@ from zengarden_database import db, Mesh, Dialogue, Entities
 #json.loads(jsonified_data)
 
 #-------------------------------------------
-mes = {
-    'Vertices' : vertices,
-    'Color' : colors,
+#mes = {
+    #'Vertices' : vertices,
+    #'Color' : colors,
 
-}
-jsonified_data = json.dumps(data2)
-json.loads(jsonified_data)
-
-#-------------------------------------------
-
-dia = {
-    'Text' : text,
-    'Option' : options,
-    'Font size' : font_size,
-    'Time' : time,
-    'Weather' : weather_type,
-    'Day time' : day_time
-
-}
-jsonified_data = json.dumps(dia)
-json.loads(jsonified_data)
+#}
+#jsonified_data = json.dumps(data2)
+#json.loads(jsonified_data)
 
 #-------------------------------------------
 
-ent = {
-    'Position' : pos,
-    'Mesh position' : mesh_ops,
+#dia = {
+    #'Text' : text,
+   # 'Option' : options,
+    #'Font size' : font_size,
+   # 'Time' : time,
+    #'Weather' : weather_type,
+    #'Day time' : day_time
+
+#}
+#jsonified_data = json.dumps(dia)
+#json.loads(jsonified_data)
+
+#-------------------------------------------
+
+#ent = {
+   # 'Position' : pos,
+  #  'Mesh position' : mesh_ops,
     #'Garden id' : garden_id,
-    'Mesh id' : mesh_id
+   # 'Mesh id' : mesh_id
 
-}
-jsonified_data = json.dumps(ent)
-json.loads(jsonified_data)
+#}
+#jsonified_data = json.dumps(ent)
+#json.loads(jsonified_data)
