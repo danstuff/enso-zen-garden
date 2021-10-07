bc9c8771528cc0929e7d8a0c3748e66b6eac669a
FangzhengDu <47069945+FangzhengDu@users.noreply.github.com>
2021-10-07 16:47:01 -0400

    update table under ERD

--- database/database.py
+++ database/database.py
@@ -7,11 +7,18 @@ cur = conn.cursor()
 cur.execute('''
 
 CREATE TABLE Dialogue (
+    id                INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    dialogue_html     STRING NOT NULL UNIQUE,
+    weather_condition STRING NOT NULL UNIQUE,
+    wind_speed_min    INTEGER NOT NULL UNIQUE,
+    wind_speed_max    INTEGER NOT NULL UNIQUE,
+    day_time_start    INTEGER NOT NULL UNIQUE,
+    day_time_length   INTEGER NOT NULL UNIQUE
+    
+);
+CREATE TABLE Mesh (
     id           INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    text         STRING  NOT NULL UNIQUE,
-    option       STRING  NOT NULL UNIQUE,
-    font_size    INTEGER NOT NULL UNIQUE,
-    time         REAL    NOT NULL UNIQUE
+    mesh_json    STRING  NOT NULL UNIQUE
 );
 
 
