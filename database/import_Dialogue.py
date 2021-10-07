f9eb7830a78c058e8c8b09f1556036cf9b9398eb
FangzhengDu <47069945+FangzhengDu@users.noreply.github.com>
2021-10-07 16:53:23 -0400

    update based on ERD

--- database/import_Dialogue.py
+++ database/import_Dialogue.py
@@ -7,11 +7,14 @@ with open('testdialogue.json', 'r') as f:
     data = json.load(f)
 
     for line in data['Dialogue']:
-        sql = "insert into Dialogue(text,option,font_size,time) values('%s',%s,%s,%s)" % (
-            line['text'],
-            line['option'],
-            line['font_size'],
-            line['time'])
+        sql = "insert into Dialogue(text,option,font_size,time) values('%s',%s,%s,%s,%s,%s)" % (
+            line['dialogue_html'],
+            line['weather_condition'],
+            line['wind_speed_min'],
+            line['wind_speed_max'],
+            line['day_time_start'],
+            line['day_time_length']
+        )
 
         connect.execute(sql)
 
