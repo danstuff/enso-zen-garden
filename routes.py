eea2c97e560bf48602e7a70ac2032fbfe22dd57f
Dan <danyost23@gmail.com>
2021-10-20 15:00:00 -0400

    Dir cleanup

--- routes.py
+++ routes.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flask import url_for
 from flask import request
 
+from modules.databaseManager import DatabaseManager
 from modules.secure import APIKeys
 
 app = Flask(__name__)
