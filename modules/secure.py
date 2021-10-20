0fa60864e3c63388e06c5aafacf2c94b64ea0756
FangzhengDu <47069945+FangzhengDu@users.noreply.github.com>
2021-10-20 14:29:15 -0400

    delete secure.py

--- modules/secure.py
+++
@@ -1,7 +0,0 @@
-class SecureData:
-    def __init__(self):
-        this.openWeather = "bf86e0c88de658dd300efbc09f6e9ab9"
-
-    def asJSON(self):
-        return json.dumps(self.__dict__)
-        
