2968624540d95bf0c0807788b2f35672809a4392
ryanjw34 <70335508+ryanjw34@users.noreply.github.com>
2021-10-25 21:44:02 -0400

    Initial Button Interaction
    Added placeholder code for what will become the functionality of the first button.

--- static/js/game/babylonInterface.js
+++ static/js/game/babylonInterface.js
@@ -86,11 +86,16 @@ class BabylonInterface {
 		var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
 
 		var button1 = BABYLON.GUI.Button.CreateSimpleButton("but1", "Place item");//button instantiation
-		//button1.width = "150px"
-		//button1.height = "40px";
-		//button1.color = "white";
-		//button1.cornerRadius = 20;
-		//button1.background = "green";
+		/* button1.width = "150px"
+		button1.height = "40px";
+		button1.color = "white";
+		button1.cornerRadius = 20;
+		button1.background = "green";
+		
+		button1.onPointerUpObservable.add(function() {
+        alert("you did it!");
+		});//end function
+		AdvancedTexture.addControl(button1); */
         const babInt = this;
 
         BABYLON.SceneLoader.ImportMeshAsync("", 
