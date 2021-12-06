457786f2f87d34b3cf6e092d7f7108582a747337
Dan <danyost23@gmail.com>
2021-12-05 19:05:41 -0500

    Mobile fixes

--- static/js/game/visualFX.js
+++ static/js/game/visualFX.js
@@ -35,7 +35,7 @@ class VisualFX {
 
         for(var i = 0; i < amount; i++) {
             //create cloud emitter
-            var cloud = new BABYLON.GPUParticleSystem("clouds", 1000);
+            var cloud = new BABYLON.GPUParticleSystem("clouds", 100);
 
             cloud.particleTexture = 
                 new BABYLON.Texture("/static/assets/textures/cloud.png");
