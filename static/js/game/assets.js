ab0c94e8857833b150eb9177a92c8fa142ce59e1
Dan <danyost23@gmail.com>
2021-12-08 04:00:43 -0500

    Added dialogues

--- static/js/game/assets.js
+++ static/js/game/assets.js
@@ -165,12 +165,226 @@ const LevelTitles = [
 
 const Dialogues = [
     { 
-        //The HTML that will be displayed when the dialogue is used
-        html: "<i>Hello World!</i>", 
+        html:
+        "\"Thunder is impressive, <br> but it is lightning that does the work.\" <br> - Mark Twain", 
 
-        //Tags are the conditions for displaying the given HTML
-        //Tags can use logical operators OR and AND, but parenthasis
-        //are not supported. If no operators are specified, AND is assumed.
-        tags: "clear or day"
+        tag: "thunderstorm"
+    },
+
+    { 
+        html:
+        "\"Every storm runs out of rain, <br> just like every dark night turns into day.\" <br> - Gary Allan", 
+
+        tag: "thunderstorm"
+    },
+
+    { 
+        html:
+        "\"Be the lightning bolt in the room: <br> the person with explosive energy which touches everyone. <br>\" - Elle Smith", 
+
+        tag: "thunderstorm"
+    },
+
+    { 
+        html:
+        "\"If it rains, let it rain, <br> if the wind blows, let it blow. <br>\" - Ikkyu", 
+
+        tag: "drizzle"
+    },
+
+    { 
+        html:
+        "\"Be like the earth. When the rain comes, <br> the earth simply opens up to the rain and soaks it all in.\" <br> - Thich Nhat Hanh", 
+
+        tag: "drizzle"
+    },
+
+    { 
+        html:
+        "\"Remember that every drop of rain that falls <br> bears into the bosom of the earth a quality of beautiful fertility.\" <br> - George Henry Lewes", 
+
+        tag: "drizzle"
+    },
+
+    { 
+        html:
+        "\"Zen is not some kind of excitement, <br> but concentration on our usual everyday routine.\" — Shunryu Suzuki", 
+
+        tag: "clear"
+    },
+
+    { 
+        html:
+        "\"As rain falls equally on the just and the unjust, <br> do not burden your heart with judgments but rain your kindness equally on all. \" <br> - Gautam Buddha", 
+
+        tag: "rain"
+    },
+
+    { 
+        html:
+        "\"In the same way that rain breaks into a house with a bad roof, <br> desire breaks into the mind that has not been practising meditation.\" <br> - Gautam Buddha", 
+
+        tag: "rain"
+    },
+
+    { 
+        html:
+        "\"Advice is like snow. <br> The softer it falls, the longer it dwells upon and the deeper in sinks into the mind.\" <br> - Samuel Taylor ", 
+
+        tag: "snow"
+    },
+
+    { 
+        html:
+        "\"Silently, like thoughts that come and go, <br> that snowflakes fall, each one a gem.\" <br> - William Hamilton Gibson", 
+
+        tag: "snow"
+    },
+
+    { 
+        html:
+        "\"Kindness is like snow - <br> it beautifies everything it covers.\" <br> - Khalil Gibran", 
+
+        tag: "snow"
+    },
+
+    { 
+        html:
+        "\"All are flowers in the sky. <br> Nameless and formless, I leave birth-and-death.\" <br> - P'ang Yun (Ho Un)", 
+
+        tag: "clear"
+    },
+
+    { 
+        html:
+        "\"Wherever you go, no matter what the weather, <br> always bring your own sunshine.\" <br> - Anthony J. D'Angelo ", 
+
+        tag: "thunderstorm"
+    },
+
+    { 
+        html:
+        "\"If you want to see the sunshine, <br> you have to weather the storm.\" <br> - Frank Lane ", 
+
+        tag: "rain"
+    },
+
+    { 
+        html:
+        "\"Clouds are on top for a reason. <br> They float so high because they refuse to carry any burden.\" <br> - Jasleen Kaur Gumber", 
+
+        tag: "clouds"
+    },
+
+    { 
+        html:
+        "\"What is eternal is the cloud drifting slowly out of sight.\" <br> - Marty Rubin", 
+
+        tag: "clouds"
+    },
+
+    { 
+        html:
+        "\"Clouds come floating into my life. <br> No longer to carrry rain or usher storm. but to add color to my sunset sky.\" <br> - Rabindranath Tagore", 
+
+        tag: "clouds"
+    },
+
+    { 
+        html:
+        "\"Every sunrise is an invitation for us to arise and brighten someone’s day.\" <br> - Jhiess Krieg", 
+
+        tag: "dawn"
+    },
+
+    { 
+        html:
+        "\"The world is always young again for just a few moments at the dawn.\" <br> - Lucy Maud Montgomery", 
+
+        tag: "dawn"
+    },
+
+    { 
+        html:
+        "\"When you arise in the morning, <br> think of what a precious privilege it is to be alive – <br> to breathe, to think, to enjoy, to love.\" <br> - Marcus Aurelius", 
+
+        tag: "morning"
+    },
+
+    { 
+        html:
+        "\"When you contemplate the big, full sunrise, <br> the more mindful and concentrated you are, the more the beauty of the sunrise is revealed to you.\" <br> - Thich Nhat Hanh", 
+
+        tag: "morning"
+    },
+
+    { 
+        html:
+        "\"Each morning we are born again. <br> What we do today is what matters most.\" <br> - Gautam Buddha", 
+
+        tag: "morning"
+    },
+
+    { 
+        html:
+        "\"The True purpose of Zen is to see things as they are, <br> and to let everything go as it goes.\" <br> - Shunryu Suzuki", 
+
+        tag: "day"
+    },
+
+    { 
+        html:
+        "\"Those who can sit perfectly physically usually take more time to obtain the tru way of Zen, <br> the actual feeling of Zen, the marrow of Zen.\" <br> - Shunryu Suzuki", 
+
+        tag: "day"
+    },
+
+    { 
+        html:
+        "\"Mindfulness helps you go home to the present. <br> And every time you go there and recognize a condition of happiness that you have, happiness comes.\" <br> - Thich Nhat Hanh", 
+
+        tag: "day"
+    },
+
+    { 
+        html:
+        "\"At the end of the day, I'm at peace, <br> because my intentions are good and my heart is pure.\" <br> - Unknown", 
+
+        tag: "evening"
+    },
+
+    { 
+        html:
+        "\"Take a deep breath, relax and let go of your worries. <br> Let the soothing essence of the night permeate and cleanse your entire being, <br> slowly drawing you into deep, relaxing slumber.\" <br> - Unknown ", 
+
+        tag: "evening"
+    },
+
+    { 
+        html:
+        "\"Begin to see what is in front of you, <br> rather than what you learned is there.\" <br> - Stephen C. Paul", 
+
+        tag: "evening"
+    },
+
+    { 
+        html:
+        "\"Night is always darker before the dawn and life is the same, <br> the hard times will pass, <br> everything will get better and sun will shine brighter than ever.\" <br>- Ernest Hemingway", 
+
+        tag: "night"
+    },
+
+    { 
+        html:
+        "\"Day is over, night has come. Today is gone, what's done is done. <br> Embrace your dreams, through the night. Tomorrow comes with a whole new light.\" <br> - George Orwell", 
+
+        tag: "night"
+    },
+
+    { 
+        html:
+        "\"Take a deep breath, relax and let go of your worries. <br> Let the soothing essence of the night permeate and cleanse your entire being, <br> slowly drawing you into deep, relaxing slumber.\" <br> - Unknown ", 
+
+        tag: "night"
     }
 ];
--- static/js/game/dialogue.js
+++ static/js/game/dialogue.js
@@ -9,39 +9,8 @@ class Dialogue {
     update(weatherData) {
         //filter the dialogues by the given weather tags
         var fDialogues = Dialogues.filter(function(d) {
-            
-            if(typeof(d.tags) !== "string") {
-                console.log(
-                    "[WARN] Tags are invalid or undefined. Ignoring: ");
-                console.log(d);
-                return false;
-            }
-
-            var terms = d.tags.split(" ");
-            var total_state;
-            var current_state;
-            var orMode = false;
-
-            for(var i in terms) {
-                var t = terms[i].trim().toLowerCase();
-
-                var current_state =
-                    (weatherData.main === t ||
-                     weatherData.day_phase_str === t);
-
-                if(t === "or")
-                    orMode = true;
-                else if(t === "and")
-                    orMode = false;
-                else if(typeof(total_state) != "boolean")
-                    total_state = current_state;
-                else if(orMode)
-                    total_state = total_state || current_state;
-                else
-                    total_state = total_state && current_state;
-            }
-
-            return total_state;
+            return d.tag == weatherData.main ||
+                d.tag == weatherData.day_phase_str;
         });
         
         if(fDialogues.length <= 0) return;
